e41504d15d9ec4e63bd8167872ba3bbce2d75721
WyattS09 <wyatt.schraeder@gmail.com>
2026-03-02 17:38:04 -0600

    Update webpack.main.config.js

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
@@ -8,4 +9,12 @@ module.exports = {
   module: {
     rules: require('./webpack.rules'),
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NEXT_PUBLIC_SUPABASE_URL': JSON.stringify('https://fkviyesjakytcjpwmpvg.supabase.co'),
+      'process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY': JSON.stringify('sb_publishable_iGDjvy9SDOaI04f8bdgohA_B0mPnWH3'),
+      'process.env.SNAPSHOT_SERVER_URL': JSON.stringify('https://szildajorbodyqjkddzx.supabase.co'), // Replace with your server URL
+      'process.env.SNAPSHOT_API_KEY': JSON.stringify('sb_publishable_4cRWlmo693rt6aPU8Tmqjg_ZDnfLWJV'), // Replace with your API key
+    }),
+  ],
 };
